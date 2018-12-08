a2d1d6a5e24e74b17ba7829c7670221690155107
babie <babie.wildheart@gmail.com>
2018-12-08 15:48:57 +0900

    test: Add Html component tests

--- test/functional/functional.test.tsx
+++ test/functional/functional.test.tsx
@@ -1,12 +1,47 @@
 /**
  * @jest-environment puppeteer
  */
+const HELMET_PARENT_CLASS_NAME = 'hyperapp-helmet-parent'
+const HELMET_CHILD_CLASS_NAME = 'hyperapp-helmet-child'
+
 describe('Test Server', () => {
   beforeAll(async () => {
     await page.goto('http://localhost:3000/b')
   })
 
-  it('should display "template#helmet-b" element on page', async () => {
+  it("should display Html component's head tags on page", async () => {
+    await expect(page).toMatchElement('head meta[charset=utf-8]')
+    await expect(page).toMatchElement(
+      'head meta[name=viewport][content="width=device-width, initial-scale=1"]'
+    )
+    await expect(page).toMatchElement('head base[href="http://localhost:3000"]')
+    await expect(page).toMatchElement(
+      'head link[rel=stylesheet][href="/html.css"]'
+    )
+    await expect(page).toMatchElement('head style', {
+      text: 'body { color: blue; }'
+    })
+    await expect(page).toMatchElement('head script[src="/html.js"][defer]')
+  })
+
+  it("should display B component's head tags on page", async () => {
+    await expect(page).toMatchElement('head title', { text: 'title-B' })
+    await expect(page).toMatchElement(
+      'head meta.helmet-b[name=description][content=description-B]'
+    )
+    await expect(page).toMatchElement(
+      'head link.helmet-b[rel=stylesheet][href="/b.css"]'
+    )
+    await expect(page).toMatchElement('head style.helmet-b', {
+      text: 'body { background-color: blue; }'
+    })
+    await expect(page).toMatchElement(
+      'head script.helmet-b[src="/b.js"][defer]'
+    )
+  })
+
+  it("should display B component's template tags on page", async () => {
     await expect(page).toMatchElement('template#helmet-b')
+    await expect(page).toMatchElement('a', { text: 'LinkToA' })
   })
 })
